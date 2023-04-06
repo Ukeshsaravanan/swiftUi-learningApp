d4c78b813176e89d2f5057e04d3bf1c0186c6dbd
Ukesh Kumar <ukeshsaravanan@gmail.com>
2023-04-06 09:42:34 +0530

    Initial Commit

---
+++ LearningAPP/LearningAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAPPApp.swift
+//  LearningAPP
+//
+//  Created by UKESH KUMAR on 06/04/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
