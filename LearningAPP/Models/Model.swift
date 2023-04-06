e0208eeb731ee87fa3ccd8ac8dffb1135703ec50
Ukesh Kumar <ukeshsaravanan@gmail.com>
2023-04-06 11:12:08 +0530

    -add assets -add data files -parsed json in content model

---
+++ LearningAPP/Models/Model.swift
@@ -0,0 +1,52 @@
+//
+//  Model.swift
+//  LearningAPP
+//
+//  Created by UKESH KUMAR on 06/04/23.
+//
+
+import Foundation
+
+struct Module :  Decodable , Identifiable {
+    
+    var id: Int
+    var category: String
+    var content: Content
+    var test: Test
+}
+
+
+struct Content: Decodable , Identifiable {
+    
+    var id: Int
+    var image: String
+    var time: String
+    var description: String
+    var lesson: [Lesson]
+}
+
+struct Lesson: Decodable , Identifiable {
+    
+    var id: Int
+    var title: String
+    var video: String
+    var duration: String
+    var explanation: String
+}
+
+struct Test: Decodable , Identifiable {
+    
+    var id : Int
+    var image : String
+    var time: String
+    var description: String
+    var questions : [Questions]
+}
+
+struct Questions : Decodable, Identifiable {
+    
+    var id: Int
+    var content: String
+    var correctIndex: Int
+    var answers: [String]
+}
